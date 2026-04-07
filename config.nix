906eb8c7496d5d42c026a52d9248ae3195137e79
Levi Nelson <54561718+LeviticusNelson@users.noreply.github.com>
2026-04-07 15:35:58 -0700

    feat: atuin + fish config for starfish and atuin

--- config.nix
+++ config.nix
@@ -3,39 +3,40 @@
     myPackages = pkgs.buildEnv {
     name = "levi-tools";
     paths =  [
-      neovim
-      fish
-      git
-      curl
-      wget
-      unzip
-      gnutar
-      gzip
-      ripgrep
-      fd
-      fzf
-      lazygit
-      go
-      gopls
-      golangci-lint
-      delve
-      gotools
-      gofumpt
-      gnumake
-      gcc
-      nodejs
-      python3
-      python3Packages.pynvim
-      docker-client
-      docker-compose
-      xclip
-      luarocks
-      lua-language-server
-      stylua
-      nil
-      nixd
-      starship
-    ];
+        neovim
+        fish
+        git
+        curl
+        wget
+        unzip
+        gnutar
+        gzip
+        ripgrep
+        fd
+        fzf
+        lazygit
+        go
+        gopls
+        golangci-lint
+        delve
+        gotools
+        gofumpt
+        gnumake
+        gcc
+        nodejs
+        python3
+        python3Packages.pynvim
+        docker-client
+        docker-compose
+        xclip
+        luarocks
+        lua-language-server
+        stylua
+        nil
+        nixd
+        starship
+        atuin
+      ];
     };
   };
 }
--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,12 @@
 set -gx EDITOR nvim
+
 fish_add_path "$HOME/.local/bin"
+
+if status is-interactive
+    atuin init fish | source
+    starship init fish | source
+    set -U fish_greeting
+    set -U fish_key_bindings fish_default_key_bindings
+    set -U fish_cursor_default line
+
+end
