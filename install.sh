5ea9833f14c6750c2aca7bfaac2730c6e3ba468e
Levi Nelson <54561718+LeviticusNelson@users.noreply.github.com>
2026-04-13 16:18:56 -0700

    tweaks again

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 cd "$(dirname "$0")" || exit 1
 
 export XDG_CONFIG_HOME="$HOME/.config"
+mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_CONFIG_HOME/nixpkgs"
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
