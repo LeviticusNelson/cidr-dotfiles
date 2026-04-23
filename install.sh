78f851b874e5e25236cacb8d0ec74c670ce81fba
Levi Nelson <54561718+LeviticusNelson@users.noreply.github.com>
2026-04-23 12:11:48 -0700

    feat: jj config

--- install.sh
+++ install.sh
@@ -6,11 +6,13 @@ cd "$(dirname "$0")" || exit 1
 export XDG_CONFIG_HOME="$HOME/.config"
 mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_CONFIG_HOME/nixpkgs"
+mkdir -p "$XDG_CONFIG_HOME/jj"
 
 ln -sf "$PWD/nvim" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$PWD/config.nix" "$XDG_CONFIG_HOME/nixpkgs/config.nix"
 ln -sf "$PWD/.bashrc" "$HOME/.bashrc"
 ln -sf "$PWD/fish" "$XDG_CONFIG_HOME/fish"
+ln -sf "$PWD/jj" "$XDG_CONFIG_HOME/jj"
 
 nix-env -iA nixpkgs.myPackages --priority 10
 
